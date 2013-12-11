fb5100ceeff2a617cb4b3cab9caa0989f7666bf2
Billy <zw963@163.com>
2013-12-11 08:51:50 +0800

    第一次重构, 生产代码抽取 TrainGraph, TrainNoRepeatRoute 两个模块, 重写了测试

--- train_route.rb
+++ train_route.rb
@@ -3,14 +3,14 @@
 
 # 求职者: 郑伟 zw963@163.com
 # 问题选择: TRAINS
-# 语言: Ruby 1.93
+# 语言: Ruby 2.0
 
 # Problem:
 #   The local commuter railroad services a number of towns in Kiwiland.
 #   Because of monetary concerns, all of the tracks are 'one-way'.
 #   That is, a route from Kaitaia to Invercargill does not imply the existence of
 #   a route from Invercargill to Kaitaia.  In fact, even if both of these routes
-#   do happen to exist, they are distinct and are not nece ssarily the same distance!
+#   do happen to exist, they are distinct and are not necessarily the same distance!
 
 #   The purpose of this problem is to help the railroad provide its customers with
 #   information about the routes. In particular, you will compute the distance along
@@ -63,73 +63,142 @@
 #         Output #9: 9
 #         Output #10: 7
 
+module TrainGraph
+  attr_accessor :graphs
 
-class TrainPath
-  attr_accessor :path
-
-  # 测试成功后, 取消注释 private.
-  # private
-
-  def initialize(graph)
-    @graph = graph
-    path_distance_hash
+  # {"AB"=>"5", "BC"=>"4" ...}
+  def train_graphs_hash
+    Hash[graphs.map {|e| e.split(/(?=\d)/)}]
   end
 
-  # {"AB"=>"5", "BC"=>"4", "CD"=>"8" ... }
-  def path_distance_hash
-    Hash[@graph.map {|e| e.split(/(?=\d)/)}]
+  # {"A"=>["AB", "AD", "AE"], "B"=>["BC"] ...}
+  def train_graph_routes_hash
+    train_graphs_hash.keys.group_by {|e| e[0] }
   end
 
-  def path_exist?
-    path_array.all? {|e| path_distance_hash.has_key? e }
+  def train_station_count
+    graphs.join.chars.to_a.uniq.join.count("A-Z")
+  end
+end
+
+module TrainNoRepeatRoute
+  include TrainGraph
+  attr_accessor :route
+
+  def route_path
+    route.chars.each_cons(2).map(&:join)
   end
 
-  # 接龙 ["AE", "EB", "BC" ... ]
-  def path_array
-    path.chars.each_cons(2).map(&:join)
+  def route_exist?
+    route_path.all? {|e| train_graphs_hash.has_key? e }
   end
 
-  def path_distance
-    return "NO SUCH ROUTE" unless path_exist?
-    path_array.inject(0) {|a, e| a = a + path_distance_hash[e].to_i }
+  def route_distance
+    return "NO SUCH ROUTE" unless route_exist?
+    route_path.inject(0) {|a, e| a = a + train_graphs_hash[e].to_i }
+  end
+end
+
+module TrainRepeatRoute
+  include TrainGraph
+  attr_accessor :route
+
+  def start_station
+    @stop_station || route[0]
   end
 
-  public :path_distance
+  def stop_station=(new_stop_station)
+    @stop_station = new_stop_station
+  end
+
+  def end_station
+    route[1]
+  end
+
+  # 假设 CC
+  #
+  # 1. 我希望获取从 C 开始的所有 path 的字符串.
+  # 2. 稍后, 我匹配, 看看有没有以 C 结束的, 有的话, 记录下来即可.
+
+  def next_station_string_array
+    train_graph_routes_hash[start_station]
+      .map {|e| start_station + e }
+      .map {|e| e.gsub(/([[:alpha:]])(\1)/, '\1') }
+  end
+
+  def parse
+    train_station_count.times do
+      stop_station = next_station_string_array
+    end
+  end
+
+  # # ["ACD", "ACE" ...]
+  # def route_path_string
+  #   # 串起来, 并替换 `连续的重复字符' 为一个.
+  #   initial_station = start_station[-1]
+  #   next_station_array = train_graph_routes_hash[]
+  #     .map {|e| route + e }
+  #     .map {|e| e.gsub(/([[:alpha:]])(\1)/, '\1') }
+  # end
+
+  # def prase
+  #   route = route_path_string
+  # end
+
+  # def start_station_char
+  #   Array[route[0]]
+  # end
+
+  # def next_station_char
+  #   route[-1]
+  # end
+
+  # def end_station_char
+  #   route[1]
+  # end
+  
+  # def matched_routes
+  #   # 假设一个站只允许经过一次, 所以, 一次遍历经过的站不会超过总数.
+  #   train_station_count.times do
+  #     # 遍历
+  #     start_station = start_station.map {|e| concat_path(e) }.flatten
+  #     # 插入一个数组.
+  #     collect_path(_initial_path, _matched_path)
+  #     # # 移除重复元素.
+  #     remove_collected_and_circle_path(_initial_path)
+  #   end
+  # end
+
 
 end
 
-class TrainRoute
-  attr_accessor :route
+class A
+  def routes
+    _matched_path = []
+    # 起点的名称数组, 例如: ['A']
+    _initial_path = Array[route[0]]
 
-  # 测试成功后, 取消注释 private.
-  # private
+    # 假设一个站只允许经过一次, 所以, 一次遍历经过的站不会超过总数.
+    train_station_count.times do
+      # 遍历
+      _initial_path = _initial_path.map {|e| concat_path(e) }.flatten
+      # 插入一个数组.
+      collect_path(_initial_path, _matched_path)
+      # # 移除重复元素.
+      remove_collected_and_circle_path(_initial_path)
+    end
 
-  def initialize(graph)
-    @graph = graph
-    path_distance_hash
+    # 对于存在回环的路径, 过路站点仍存在经过两次的可能性, 例如: AE, 移除掉ADCDE
+    _matched_path.flatten.reject {|e| e.chars.to_a[1..-2].uniq! }
   end
 
-  # {"AB"=>"5", "BC"=>"4", "CD"=>"8" ... }
-  def path_distance_hash
-    Hash[@graph.map {|e| e.split(/(?=\d)/)}]
-  end
+  # def Distance(path)
+  #   path.chars.each_cons(2).map(&:join).inject(0) {|a, e| a = a + path_distance_hash[e].to_i }
+  # end
 
-  # {"A"=>["AB", "AD", "AE"], "B"=>["BC"], ...}
-  def route_path_hash
-    path_distance_hash.keys.group_by {|e| e[0] }
-  end
-
-  def Distance(path)
-    path.chars.each_cons(2).map(&:join).inject(0) {|a, e| a = a + path_distance_hash[e].to_i }
-  end
-
-  def Distance_all(path_array)
-    path_array.map {|e| Distance(e) }
-  end
-
-  def station_quantity
-    @graph.join.chars.to_a.uniq.join.count("A-Z")
-  end
+  # def Distance_all(path_array)
+  #   path_array.map {|e| Distance(e) }
+  # end
 
   def collect_path(path_array, matched_path_array)
     _dest_station = route[1]
@@ -148,32 +217,6 @@ class TrainRoute
     path_array
   end
 
-  def concat_path(path)
-    # 获取 path 最后一个 char.
-    _next_station = path.chars.to_a.last
-    # 串起来, 并替换连续的重复字符为一个.
-    route_path_hash[_next_station].map {|e| path + e }.map {|e| e.gsub(/([[:alpha:]])(\1)/, '\1') }
-  end
-
-  def routes
-    _matched_path = []
-    # 起点的名称数组, 例如: ['A']
-    _initial_path = Array[route[0]]
-
-    # 假设一个站只允许经过一次, 所以, 一次遍历经过的站不会超过总数.
-    station_quantity.times do
-      # 遍历
-      _initial_path = _initial_path.map {|e| concat_path(e) }.flatten
-      # 插入一个数组.
-      collect_path(_initial_path, _matched_path)
-      # # 移除重复元素.
-      remove_collected_and_circle_path(_initial_path)
-    end
-
-    # 对于存在回环的路径, 过路站点仍存在经过两次的可能性, 例如: AE, 移除掉ADCDE
-    _matched_path.flatten.reject {|e| e.chars.to_a[1..-2].uniq! }
-  end
-
   #  block 版, 需从外部传入终止条件.
   def routes_while_distance(&block)
     _matched_path = []
@@ -188,7 +231,7 @@ class TrainRoute
     (_matched_path.flatten.map {|e| Distance(e) }.select &block).size
   end
 
-  # 
+  #
   def routes_while_stop(stops = 0, &block)
     _matched_path = []
     _initial_path = Array[route[0]]
@@ -218,48 +261,22 @@ class TrainRoute
 
 end
 
-graph = %w(AB5 BC4 CD8 DC8 DE6 AD5 CE2 EB3 AE7)
-new_path = TrainPath.new(graph)
+# # The number of trips starting at C and ending at C with a maximum of 3 stops.
+# new_route.route = "CC"
+# p new_route.routes_while_stop {|e| e <= 3 }     # => 2
 
-# The distance of the route A-B-C
-new_path.path = 'ABC'
-p new_path.path_distance                # => 9
+# # # The number of trips starting at A and ending at C with exactly 4 stops
+# new_route.route = "AC"
+# p new_route.routes_while_stop(4)                # => 3
 
-# The distance of the route A-D.
-new_path.path = "AD"
-p new_path.path_distance                # => 5
+# # # The length of the shortest route from A to C.
+# new_route.route = "AC"
+# p new_route.routes_distance.min                 # => 9
 
-# The distance of the route A-D-C.
-new_path.path = "ADC"
-p new_path.path_distance                # => 13
+# # # The length of the shortest route from B to B.
+# new_route.route = "BB"
+# p new_route.routes_distance.min                 # => 9
 
-# The distance of the route A-E-B-C-D.
-new_path.path = "AEBCD"
-p new_path.path_distance                # => 22
-
-# The distance of the route A-E-D.
-new_path.path = "AED"
-p new_path.path_distance                # => "NO SUCH ROUTE"
-
-graph = %w(AB5 BC4 CD8 DC8 DE6 AD5 CE2 EB3 AE7)
-new_route = TrainRoute.new(graph)
-
-# The number of trips starting at C and ending at C with a maximum of 3 stops.
-new_route.route = "CC"
-p new_route.routes_while_stop {|e| e <= 3 }     # => 2
-
-# # The number of trips starting at A and ending at C with exactly 4 stops
-new_route.route = "AC"
-p new_route.routes_while_stop(4)                # => 3
-
-# # The length of the shortest route from A to C.
-new_route.route = "AC"
-p new_route.routes_distance.min                 # => 9
-
-# # The length of the shortest route from B to B.
-new_route.route = "BB"
-p new_route.routes_distance.min                 # => 9
-
-# # The number of different routes from C to C with a distance of less than 30.
-new_route.route = "CC"
-p new_route.routes_while_distance {|distance| distance < 30 } # => 7
+# # # The number of different routes from C to C with a distance of less than 30.
+# new_route.route = "CC"
+# p new_route.routes_while_distance {|distance| distance < 30 } # => 7
